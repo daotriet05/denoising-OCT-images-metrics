b1a160d532fc2d1cfab339c3ac436d81055731dc
Triet Dao <67148915+daotriet05@users.noreply.github.com>
2024-06-06 11:44:18 +0700

    update CNR

--- MSR_CNR_ENL.py
+++ MSR_CNR_ENL.py
@@ -22,37 +22,28 @@ def comp_MSR_CNR_ENL(im_out,ref, pos, background_indices):
     foreground_indices = np.setdiff1d(np.arange(N), background_indices)
 
     # ===========
-    # Compute MSR
-    # MSR = average of mean to standard deviation ratios on foreground regions
 
     f_mean = 0  # mean of intensity values in the foreground ROIs
     f_std = 0   # std of intensity values in the foreground ROIs
     MSR = 0
+    CNR = 0
 
+    # background ROI vectors
+    broi = np.array([roi[idx] for idx in background_indices])
+    m_broi = np.mean(broi)
+    std_broi = np.std(broi)
+
+    # mean and std foreground ROI
     for j in foreground_indices:
         m = np.mean(roi[j])
         s = np.std(roi[j])
-        MSR += m / s
-        f_mean += m
-        f_std += s
+        MSR += m / s 
+        CNR+=10*np.log(abs(m-m_broi) / np.sqrt(0.5 * (s**2+std_broi**2) ))
 
     Nf = len(foreground_indices)  # number of foreground regions
 
     MSR /= Nf
-    f_mean /= Nf
-    f_std /= Nf
-
-    # ===========
-    # Compute CNR
-    # CNR = measures the contrast between foreground regions and background regions.
-
-    # background ROI vectors
-    broi = np.array([roi[idx] for idx in background_indices])
-
-    CNR = abs(f_mean - np.mean(broi)) / np.sqrt(0.5 * (f_std**2 + np.std(broi)**2))
-
-    # ===========
-    # Compute ENL
-    ENL = np.mean(broi)**2 / np.std(broi)**2
+    CNR /= Nf
+    ENL = m_broi**2 / std_broi**2
 
     return MSR, CNR, ENL
